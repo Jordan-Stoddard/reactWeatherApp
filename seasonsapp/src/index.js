0b4e7e154734d71e5e46291279dbb1da65e02784
Jordan Stoddard <jjsincorporated@gmail.com>
2019-01-26 16:27:53 -0800

    refactored to class component, initialized state

--- seasonsapp/src/index.js
+++ seasonsapp/src/index.js
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
 
-const App = () => {
-    return (
-        <div>Hi there!</div>
-    )
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            lat: null,
+        }
+    }
+
+
+    // React says we have to define render
+ render() {
+    window.navigator.geolocation.getCurrentPosition(
+        position => console.log(position),
+        err => console.log(err)
+      );
+
+     return (
+         <div>Latitude: </div>
+     )
+ }
 }
 
-ReactDOM.render(<App/>, document.querySelector('#root'))
+
+ReactDOM.render(<App />, document.querySelector("#root"));
