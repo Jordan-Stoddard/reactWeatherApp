6388b237740fe40cf3fb0acac1cab11958bfe03f
Jordan Stoddard <jjsincorporated@gmail.com>
2019-01-26 16:53:09 -0800

    set the lat property on state to the geolocation latitude

--- seasonsapp/src/index.js
+++ seasonsapp/src/index.js
@@ -8,18 +8,24 @@ class App extends React.Component {
         this.state = {
             lat: null,
         }
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                // we called setState!
+                this.setState({lat: position.coords.latitude})
+            },
+            err => console.log(err)
+          );
     }
+
+
 
 
     // React says we have to define render
  render() {
-    window.navigator.geolocation.getCurrentPosition(
-        position => console.log(position),
-        err => console.log(err)
-      );
+  
 
      return (
-         <div>Latitude: </div>
+         <div>Latitude: {this.state.lat}</div>
      )
  }
 }
