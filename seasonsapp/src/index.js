54dd3406a1a7cee0c4819f7e030c47c0acbfa993
Jordan Stoddard <jjsincorporated@gmail.com>
2019-01-28 13:18:14 -0800

    finishing touches

--- seasonsapp/src/index.js
+++ seasonsapp/src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from './SeasonDisplay.jsx'
+import Spinner from './Spinner'
 
 class App extends React.Component {
   state = {lat: null, errorMessage: ''}
 
+
+//-------------------------------//
   componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
       position => this.setState({ lat: position.coords.latitude }),
@@ -12,9 +15,7 @@ class App extends React.Component {
     );
   }
 
-
-
-  render() {
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     }
@@ -23,7 +24,16 @@ class App extends React.Component {
       return <SeasonDisplay lat={this.state.lat}/>;
     }
 
-    return <div>Loading...</div>;
+    return <div><Spinner message="Please accept location request."/></div>;
+  }
+
+
+  render() {
+    return (
+      <div className="border red">
+      {this.renderContent()}
+      </div>
+    )
   }
 }
 
